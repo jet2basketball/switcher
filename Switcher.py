b278f3776ea90b9ad491503469da16e2dc0b7dd2
Tejavi <jetkumar2@gmail.com>
2021-06-21 16:30:16 -0400

    Updated to allow user to input letter instead of index

--- Switcher.py
+++ Switcher.py
@@ -1,28 +1,74 @@
-def switch_letters():
+
+#converts a string into a list of its characters
+def list_of_letters(word):
     letters = []
-    
-    str = input('pick a word ')
-    index_one = int(input('index of first letter '))
-    index_two = int(input('index of second letter '))
 
+    for letter in word:
+        letters.append(letter)
 
+    return letters
+
+#converts a list of characters into a string 
+def list_to_string(list):
     new_str = ""
 
-    for letter in str:
-        letters.append(letter)
-    
-    char_at_index_one = letters[index_one]
-    char_at_index_two = letters[index_two]
-
-    letters[index_one] = char_at_index_two
-    letters[index_two] = char_at_index_one
-
-    for character in letters:
+    for character in list:
         new_str += character
-
+    
     return new_str
 
 
-print(switch_letters())
+def swap_letters():
+
+    index_one = 0
+    index_two = 0
+
+    word = input('Pick a word ')
+    letters = list_of_letters(word)
+
+    #finding index of first letter 
+    letter_one = input('Pick a letter ')
+    if letters.count(letter_one.lower()) > 1:
+        indices = []
+
+        for i in range(len(letters)):
+            if letters[i] == letter_one.lower():
+                indices.append(i)
+
+        number = int(input('Which one(1,2,3,etc) '))
+
+        index_one += indices[number - 1]
+
+    else:
+        index = letters.index(letter_one)
+        index_one += index
+
+    #finding index of second letter 
+    letter_two = input('Pick another letter ')
+    if letters.count(letter_two.lower()) > 1:
+        indices = []
+
+        for i in range(len(letters)):
+            if letters[i] == letter_two.lower():
+                indices.append(i)
+
+        number = int(input('Which one(1,2,3,etc) '))
+        index_two += indices[number - 1]
+
+    else:
+        index = letters.index(letter_two)
+        index_two += index
+
+
+    #swapping the two letters based on their index 
+    letters[index_one] = letter_two
+    letters[index_two] = letter_one
+
+    #converting the list of characters back into a string
+    updated_word = list_to_string(letters)
+
+    return updated_word
+
+print(swap_letters())
 
 
